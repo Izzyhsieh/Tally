9be33e52bcaacc4796cdb2aca73f592734035cd4
izzyhsieh <oplk.izzy@gmail.com>
2015-01-14 16:38:06 +0800

    fine tune UI - main page action bar change as icons - change post button - add personal page default msg

--- src/com/example/tally/ViewPagerActivity.java
+++ src/com/example/tally/ViewPagerActivity.java
@@ -1,10 +1,14 @@
 package com.example.tally;
 
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff.Mode;
+import android.graphics.PorterDuffColorFilter;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.view.ViewPager;
 import android.support.v4.view.ViewPager.OnPageChangeListener;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 
@@ -12,6 +16,11 @@ import android.widget.LinearLayout;
 public class ViewPagerActivity extends FragmentActivity {
 	private ViewPager _mViewPager;
 	private ViewPagerAdapter _adapter;
+	private ImageView mainPage;
+	private ImageView personalPage;
+	
+	//color filter
+	ColorFilter colorFilter = new PorterDuffColorFilter(R.color.l_blue, Mode.SRC_IN);
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -26,7 +35,7 @@ public class ViewPagerActivity extends FragmentActivity {
      _mViewPager.setAdapter(_adapter);
 	 _mViewPager.setCurrentItem(0);
 	 
-	 LinearLayout _mFirstTab = (LinearLayout) findViewById(R.id.first_text);
+	 LinearLayout _mFirstTab = (LinearLayout) findViewById(R.id.first_text);	 
 	 _mFirstTab.setOnClickListener(new View.OnClickListener() {
 		
 		@Override
@@ -49,23 +58,28 @@ public class ViewPagerActivity extends FragmentActivity {
     }
     private void setTab(){
 			_mViewPager.setOnPageChangeListener(new OnPageChangeListener(){
-			    		
 						@Override
 						public void onPageScrollStateChanged(int position) {}
 						@Override
 						public void onPageScrolled(int arg0, float arg1, int arg2) {}
 						@Override
 						public void onPageSelected(int position) {
+							mainPage = (ImageView) findViewById(R.id.main_page);
+					        personalPage = (ImageView) findViewById(R.id.personal_page);
 							// TODO Auto-generated method stub
 							switch(position){
 							case 0:
-								findViewById(R.id.first_tab).setVisibility(View.VISIBLE);
-								findViewById(R.id.second_tab).setVisibility(View.INVISIBLE);
+								//findViewById(R.id.first_tab).setVisibility(View.VISIBLE);
+								//findViewById(R.id.second_tab).setVisibility(View.INVISIBLE);
+								mainPage.setColorFilter(colorFilter);
+								personalPage.setColorFilter(null);
 								break;
 								
 							case 1:
-								findViewById(R.id.first_tab).setVisibility(View.INVISIBLE);
-								findViewById(R.id.second_tab).setVisibility(View.VISIBLE);
+								//findViewById(R.id.first_tab).setVisibility(View.INVISIBLE);
+								//findViewById(R.id.second_tab).setVisibility(View.VISIBLE);
+								mainPage.setColorFilter(null);
+								personalPage.setColorFilter(colorFilter);
 								break;
 							}
 						}
